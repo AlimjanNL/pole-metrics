9876151f04cea8d604675704f938b3035679091a
Alimjan Ablikim <alimjan@Alimjans-MBP.home>
2024-04-30 21:21:24 +0200

    Initialize project

---
+++ src/main/java/nl/alimjan/polemetrics/PoleMetricsApplication.java
@@ -0,0 +1,13 @@
+package nl.alimjan.polemetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoleMetricsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PoleMetricsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nl/alimjan/polemetrics/PoleMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.alimjan.polemetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoleMetricsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
